eacb4df6cd68f4e043b6bf67253ca6118fa3fc95
guoyixin <yx.guo@yunding360.com>
2019-10-09 15:06:30 +0800

    初始化spring-boot-fun

---
+++ spring-boot-fun/src/main/java/me/theembers/SpringBootFunApplication.java
@@ -0,0 +1,19 @@
+package me.theembers;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * @author TheEmbers Guo
+ * createTime 2019-10-09 14:56
+ */
+@SpringBootApplication(scanBasePackages = "me.theembers.fun")
+public class SpringBootFunApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .banner(new TheEmbersBanner()).bannerMode(Banner.Mode.LOG)
+                .sources(SpringBootFunApplication.class)
+                .run(args);
+    }
+}
