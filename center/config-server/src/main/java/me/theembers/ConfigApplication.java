37475893d91ebe8187751524ef60ce0ad705b096
guoyixin <yx.guo@yunding360.com>
2019-10-10 17:55:06 +0800

    rebuild all

---
+++ center/config-server/src/main/java/me/theembers/ConfigApplication.java
@@ -0,0 +1,19 @@
+package me.theembers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author TheEmbers Guo
+ * createTime 2019-10-10 17:42
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
---
+++ center/eureka-server/src/main/java/me/theembers/EurekaApplication.java
@@ -0,0 +1,17 @@
+package me.theembers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author TheEmbers Guo
+ * createTime 2019-10-10 17:35
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
