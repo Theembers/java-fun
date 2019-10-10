e7efe5f9b7dfe595a65b14922862ff60ec0974a6
guoyixin <yx.guo@yunding360.com>
2019-10-10 18:17:54 +0800

    spring-cloud-config

--- center/config-server/src/main/java/me/theembers/ConfigApplication.java
+++ center/config-server/src/main/java/me/theembers/ConfigApplication.java
@@ -2,15 +2,15 @@ package me.theembers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author TheEmbers Guo
  * createTime 2019-10-10 17:42
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableConfigServer
 public class ConfigApplication {
     public static void main(String[] args) {
