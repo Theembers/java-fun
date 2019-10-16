1d0e3887882b97c3779f8443cb47e775a316ba25
Theembers <unusual7@163.com>
2019-10-16 10:29:32 +0800

    commit all change

--- server/api-gateway/src/main/java/me/theembers/controller/NoRefreshConfigController.java
+++ server/api-gateway/src/main/java/me/theembers/controller/NoRefreshConfigController.java
@@ -1,27 +1,23 @@
 package me.theembers.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author TheEmbers Guo
- * createTime 2019-10-11 11:35
+ * createTime 2019-10-12 11:54
  */
-
-@RefreshScope
 @RestController
-@RequestMapping(value = "config")
-public class ConfigInfoController {
+@RequestMapping(value = "no-refreshed-config")
+public class NoRefreshConfigController {
 
     @Value("${api-version}")
     private String apiVersion;
 
-    @GetMapping("/api-version")
+    @GetMapping("api-version")
     public String getApiVersion() {
         return apiVersion;
     }
-
 }
