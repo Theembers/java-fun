5384ddfdb082c35f05b6afb07796280299fd929c
Theembers <unusual7@163.com>
2019-12-16 16:09:53 +0800

    sub1

--- lab/src/main/java/me/theembers/homework/subject1/DemoClient.java
+++ lab/src/main/java/me/theembers/homework/subject1/DemoClient.java
@@ -8,36 +8,36 @@ public class DemoClient {
         /**
          * integer 的例子
          */
-//        IntegerAntMinStack minStack = new IntegerAntMinStack();
-//        Random random = new Random(31);
-//        for (int i = 0; i < 10; i++) {
-//            int item = random.nextInt(100);
-//            minStack.push(item);
-//            System.out.println("Push: " + item);
-//            boolean flag = random.nextBoolean();
-//            if (flag && !minStack.empty()) {
-//                try {
-//                    int min = minStack.min();
-//                    System.out.println("Peek Min: " + min);
-//                } catch (Exception e) {
-//                    System.err.println();
-//                }
-//            }
-//        }
+        IntegerAntMinStack integerAntMinStack = new IntegerAntMinStack();
+        Random random1 = new Random(31);
+        for (int i = 0; i < 10; i++) {
+            int item = random1.nextInt(100);
+            integerAntMinStack.push(item);
+            System.out.println("Push: " + item);
+            boolean flag = random1.nextBoolean();
+            if (flag && !integerAntMinStack.empty()) {
+                try {
+                    int min = integerAntMinStack.min();
+                    System.out.println("Peek Min: " + min);
+                } catch (Exception e) {
+                    System.err.println();
+                }
+            }
+        }
 
         /**
          * string 的例子
          */
-        StringAntMinStack minStack = new StringAntMinStack();
-        Random random = new Random(66);
+        StringAntMinStack stringAntMinStack = new StringAntMinStack();
+        Random random2 = new Random(66);
         for (int i = 0; i < 10; i++) {
-            String item = String.valueOf(random.nextInt(100));
-            minStack.push(item);
+            String item = String.valueOf(random2.nextInt(100));
+            stringAntMinStack.push(item);
             System.out.println("Push: " + item);
-            boolean flag = random.nextBoolean();
-            if (flag && !minStack.empty()) {
+            boolean flag = random2.nextBoolean();
+            if (flag && !stringAntMinStack.empty()) {
                 try {
-                    String min = minStack.min();
+                    String min = stringAntMinStack.min();
                     System.out.println("Peek Min: " + min);
                 } catch (Exception e) {
                     System.err.println();
