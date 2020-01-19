6669a7fdc4011c10152a336902e23844b398c433
Theembers <unusual7@163.com>
2020-01-19 19:27:12 +0800

    add

--- lab/src/main/java/me/theembers/jdk/ListNote.java
+++ lab/src/main/java/me/theembers/jdk/ListNote.java
@@ -1,8 +1,11 @@
 package me.theembers.jdk;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ListNote {
     static final List<Object> arrayList = new ArrayList<>();
+    static final List<Object> linkList = new LinkedList<>();
+
 }
