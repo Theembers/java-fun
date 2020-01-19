bdac9699577c74d187a4629fd73dcca0dce2d853
Theembers <unusual7@163.com>
2020-01-19 11:15:14 +0800

    add

--- lab/src/main/java/me/theembers/anytest/SetDemo.java
+++ lab/src/main/java/me/theembers/anytest/SetDemo.java
@@ -1,7 +1,10 @@
 package me.theembers.anytest;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetDemo {
     public static void main(String[] args) {
-
+        Set<String> set = new HashSet<>();
     }
 }
